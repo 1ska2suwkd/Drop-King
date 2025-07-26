81de25e2df5836aa5c13208dd8950b109dbce979
1ska2suwkd <mickey050110@naver.com>
2025-07-26 09:13:23 +0900

    Update MainScene.js

--- 2D/Drop King/js/scenes/MainScene.js
+++ 2D/Drop King/js/scenes/MainScene.js
@@ -9,14 +9,33 @@ export default class MainScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(
-            this.sys.game.config.width / 2,
-            this.sys.game.config.height / 2,
-            'background1');
+        //충돌 영역이 있는 배경 생성하기
+        const map = this.make.tilemap({ key: 'map' });
+        this.add.image(150, -25, 'background1').setOrigin(0, 0);
 
+        this.ground = this.physics.add.staticGroup();
+
+        const groundLayer = map.getObjectLayer('collision');
+
+        if (groundLayer && groundLayer.objects) {
+            groundLayer.objects.forEach(obj => {
+                const ground = this.ground.create(
+                    obj.x + obj.width / 2,
+                    obj.y - obj.height / 2,
+                    null
+                );
+                ground.setSize(obj.width, obj.height).setVisible(false).refreshBody();
+            });
+        }
+
+        if (this.ground && this.ground.getLength() > 0) {
+            this.physics.add.collider(this.player, this.ground);
+        }
+
+        //플레이어, 애니메이션 생성
         this.player = this.physics.add.sprite(
             this.sys.game.config.width / 2,
-            this.sys.game.config.height - 33,
+            this.sys.game.config.height - 100,
             'player'
         ).setScale(1.5);
 
@@ -28,12 +47,14 @@ export default class MainScene extends Phaser.Scene {
         })
         this.anims.create({
             key: 'walk',
-            frames: this.anims.generateFrameNumbers('player', {start: 1, end: 3}),
+            frames: this.anims.generateFrameNumbers('player', { start: 1, end: 3 }),
             frameRate: 10,
             repeat: -1
         })
         //생성하면 idle 재생
         this.player.play('idle');
+
+
         this.cursors = this.input.keyboard.createCursorKeys();
         //spacekey를 새로 정의하는 이유는 Justup을 사용하기 위함이다.
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -45,7 +66,7 @@ export default class MainScene extends Phaser.Scene {
 
         const speed = 150;
 
-        if(this.cursors.left.isDown) {
+        if (this.cursors.left.isDown) {
             this.player.setVelocityX(-speed);
             this.player.anims.play('walk', true);
             this.player.setFlipX(true);
@@ -60,10 +81,10 @@ export default class MainScene extends Phaser.Scene {
 
         //점프 차징
         //this.player.body.bloked.down은 플레이어가 땅에 닿아있는 지 확인하는 것
-        if(this.spaceKey.isDown && this.player.body.bloked.down) {
+        if (this.spaceKey.isDown && this.player.body.blocked.down) {
             this.isChargingJump = true;
             this.jumpPower += 10;
-            if (this.jumpPower > this.maxJumpPower){
+            if (this.jumpPower > this.maxJumpPower) {
                 this.jumpPower = this.maxJumpPower;
             }
             this.player.setFrame(4);
@@ -71,14 +92,14 @@ export default class MainScene extends Phaser.Scene {
 
         //점프 발사
         //Justup은 손 뗀 순간을 의미
-        if(this.isChargingJump && Phaser.Input.Keyboard.Justup(this.spaceKey)) {
+        if (this.isChargingJump && Phaser.Input.Keyboard.Justup(this.spaceKey)) {
             this.isChargingJump = false;
             this.player.setVelocityY(-this.jumpPower)
             this.jumpPower = 0
             this.player.setFrame(5);
         }
 
-        if(!this.player.body.blocked.down && this.player.body.velocity.y > 0 && !this.isChargingJump) {
+        if (!this.player.body.blocked.down && this.player.body.velocity.y > 0 && !this.isChargingJump) {
             this.player.setFrame(6);
         }
     }
