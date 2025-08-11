a9c58aaec0a5a1b801ade170fe3d464b46419385
1ska2suwkd <mickey050110@naver.com>
2025-08-11 14:37:44 +0000

    StartScene proximityPopup 클래스 리펙토링

--- Drop King/js/scenes/StartScene.js
+++ Drop King/js/scenes/StartScene.js
@@ -1,43 +1,24 @@
 import { PlayerController } from "../playerController.js";
 
 class ProximityPopup extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, textureKey, message, opts = {}) {
+    constructor(scene, x, y, textureKey) {
         super(scene, x, y);
         scene.add.existing(this);
 
-        this.opts = Object.assign({
-            showRadius: 180,   // 이 거리 안: 썸네일 등장
-            revealRadius: 90,  // 이 거리 안: 크게 + 텍스트 표시
-            hideRadius: 180,   // 이 거리 밖: 전부 숨김
-            thumbScale: 0.4,   // 썸네일 크기
-            fullScale: 0.4,    // 확대 크기
-            tweenTime: 220     // 트윈 시간(ms)
-        }, opts);
+        this.openRadius = 180;      // 보이는 범위
+        this.hideRadius = 180;      // 숨김 범위
+        this.fullScale = 0.4;       // 크기
+        this.tweenTime = 220;       // 트윈 시간(ms)
 
         this.img = scene.add.image(0, 0, textureKey)
             .setScale(0.001)
             .setAlpha(0)
             .setOrigin(0.5);
 
-        const style = {
-            fontFamily: '"Press Start 2P", sans-serif',
-            fontSize: '14px',
-            align: 'center',
-            resolution: 2,
-            color: '#ffffff',
-            stroke: '#000000',
-            strokeThickness: 4,
-            wordWrap: { width: 260, useAdvancedWrap: true }
-        };
 
-        // 이미지 아래쪽에 메시지
-        this.msg = scene.add.text(0, this.img.height / 2 + 16, message, style)
-            .setOrigin(0.5, 0)
-            .setAlpha(0);
-
-        this.add([this.img, this.msg]);
+        this.add(this.img);
         this.setDepth(1000);
-        this.state = 'hidden'; // 'hidden' | 'thumb' | 'open'
+        this.state = 'hidden'; // 'hidden' | 'open'
     }
 
     update(player) {
@@ -46,61 +27,43 @@ class ProximityPopup extends Phaser.GameObjects.Container {
         if (!Phaser.Geom.Intersects.RectangleToRectangle(this.getBounds(), cam.worldView)) return;
 
         const d = Phaser.Math.Distance.Between(player.x, player.y, this.x, this.y);
-        if (this.state === 'hidden' && d < this.opts.showRadius) {
-            this.#toThumb();
-        } else if (this.state === 'thumb') {
-            if (d < this.opts.revealRadius) this.#toOpen();
-            else if (d > this.opts.hideRadius) this.#toHidden();
-        } else if (this.state === 'open') {
-            if (d > this.opts.hideRadius) this.#toHidden();
+        if (this.state === 'hidden' && d < this.openRadius) {
+            this.#toOpen();
+        } else if (this.state === 'open' && d > this.hideRadius) {
+            this.#toHidden();
         }
     }
 
     getBounds() {
-        const r = Math.max(this.opts.hideRadius, 120);
+        const r = Math.max(this.hideRadius, 120);
         return new Phaser.Geom.Rectangle(this.x - r, this.y - r, r * 2, r * 2);
     }
 
     #killTweens() {
         this.scene.tweens.killTweensOf(this.img);
-        this.scene.tweens.killTweensOf(this.msg);
     }
-    #toHidden() {
+
+    #setState(state){
         this.#killTweens();
-        this.state = 'hidden';
+        this.state = state;
+    }
+
+    #toHidden() {
+        this.#setState('hidden');
         this.scene.tweens.add({
-            targets: [this.img, this.msg],
+            targets: this.img,
             alpha: 0,
-            duration: this.opts.tweenTime,
+            duration: this.tweenTime,
             onComplete: () => this.img.setScale(0.001)
         });
     }
-    #toThumb() {
-        this.#killTweens();
-        this.state = 'thumb';
-        this.img.setAlpha(0);
-        this.img.setScale(0.001);
-        this.msg.setAlpha(0);
-        this.scene.tweens.add({
-            targets: this.img,
-            alpha: 0.85,
-            scale: this.opts.thumbScale,
-            duration: this.opts.tweenTime
-        });
-    }
     #toOpen() {
-        this.#killTweens();
-        this.state = 'open';
+        this.#setState('open');
         this.scene.tweens.add({
             targets: this.img,
             alpha: 1,
-            scale: this.opts.fullScale,
-            duration: this.opts.tweenTime
-        });
-        this.scene.tweens.add({
-            targets: this.msg,
-            alpha: 1,
-            duration: this.opts.tweenTime
+            scale: this.fullScale,
+            duration: this.tweenTime
         });
     }
 }
@@ -137,11 +100,11 @@ export default class StartScene extends Phaser.Scene {
         this.controller = new PlayerController(this, 333, 433);
         this.physics.add.collider(this.controller.player, this.ground);
 
-        // === [ADD] 거리 팝업들 ===
         // 위치(x,y)와 메시지는 원하는 곳으로 바꿔서 쓰면 됨.
         this.popups = [
-            new ProximityPopup(this, 480, 310, 'controls',),
-        ];
+    new ProximityPopup(this, 480, 310, 'controls'),
+    // new ProximityPopup(this, x, y, '다른텍스처')
+  ];
     }
 
     update() {
@@ -159,11 +122,11 @@ export default class StartScene extends Phaser.Scene {
 
         // [ADD] 팝업 업데이트
         const player = this.controller.player;
-        if (this.popups) {
-            for (let i = 0; i < this.popups.length; i++) {
-                this.popups[i].update(player);
-            }
-        }
+         if (this.popups) {
+    for (let i = 0; i < this.popups.length; i++) {
+        this.popups[i].update(player);
+    }
+}
 
         // 텐트 안으로 떨어지면 인트로 씬으로 변경
         if (player.y > 650) {
